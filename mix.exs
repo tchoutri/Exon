e39c763f7fd3fc1270e257cdd81992375574c94d
Théophile Choutri <theophile@choutri.eu>
2016-01-02 16:44:04 +0100

    changed the version.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Exon.Mixfile do
 
   def project do
     [app: :exon,
-     version: "0.0.1",
+     version: "2.0.1",
      elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
