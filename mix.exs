484ee66fcd076bd37bfe3d024514fe494165e578
Théophile Choutri <theophile@choutri.eu>
2016-06-18 22:48:42 +0200

    updated credo version

--- mix.exs
+++ mix.exs
@@ -29,9 +29,10 @@ defmodule Exon.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-     {:credo, "~> 0.3", only: :dev},
+     {:credo, "~> 0.4", only: :dev},
      {:phoenix, "~> 1.1"},
      {:phoenix_ecto, "~> 2.0"},
+     {:qrcode, git: "https://gitlab.com/Pacodastre/qrcode"},
      {:sqlite_ecto, "~> 1.0"},
      {:phoenix_html, "~> 2.3"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
