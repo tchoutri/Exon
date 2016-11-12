0a411cc1cc8e09ed47b9fc906b60e2f00155c68d
Théophile Choutri <theophile@choutri.eu>
2016-11-12 11:55:28 +0100

    update the web UI

--- web/router.ex
+++ web/router.ex
@@ -18,8 +18,8 @@ defmodule Exon.Router do
 
     get "/", PageController, :index
     get "/about", PageController, :about
-    get "/item/:id", PageController, :id # *TODO* : This should not return raw JSON. Let's check it with the headesr.
-    get "/item/:id/qrcode", PageController, :qrcode
+    get "/item/:id", PageController, :id # *TODO* : This should *not* return raw JSON. Let's check it with the headesr.
+    get "/item/:id/qrcode.png", PageController, :qrcode
   end
 
   scope "/form", Exon do
